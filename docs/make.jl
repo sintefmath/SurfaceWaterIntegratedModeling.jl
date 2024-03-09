34dd893477e226628af201a703244c58233da28e
Odd <odd.andersen@sintef.no>
2024-03-09 19:00:33 +0100

    Modified docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ function set_to_cairo(content)
     return content
 end
 
-Literate.markdown("../examples/urban.jl", "src/"; execute=false, preprocess = set_to_cairo)
+Literate.markdown("examples/urban.jl", "src/"; execute=false, preprocess = set_to_cairo)
 Literate.markdown("../examples/flat_areas.jl", "src/"; execute=false, preprocess = set_to_cairo)
 Literate.markdown("../examples/synthetic.jl", "src/"; execute=false, preprocess = set_to_cairo)
 
